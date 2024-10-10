33411f46d643e600591b7a366e2c0a073e5ed9a7
fmajoyegbe@gmail.com <fola@Folas-MBP.lan>
2024-10-10 17:55:33 +0100

    Merged remote changes

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -13,15 +13,15 @@ import random
 
 #Ask: roll the dice
 
-number_of_rolls = 0
+
 
 while True: 
    choice = input("Roll the dice? (y/n): ").lower()
    if choice == "y":
       die1 = random.randint(1, 6)
       die2 = random.randint(1, 6)
-      number_of_rolls += 1
-      print(f'({die1}, {die2}), {number_of_rolls}')
+
+      print(f'({die1}, {die2}),')
       
    elif choice == "n":
       print('Thanks for playing!')
